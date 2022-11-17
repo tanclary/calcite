fda1ff8c661eb4bc5a24d1c312a845a317a17e11
TJ Banghart <66929211+tjbanghart@users.noreply.github.com>
2022-11-17 08:38:47 -0800

    [CALCITE-5621] Support UDT declarations from root of schema model. (#7)
    Related to CALCITE-5346.
    
    Previously we relied on the parser to map unrecognized datatypes to a known type using SqlAlienSystemTypeNameSpec. This worked but made it difficult to change or add new types as necessary. One would have to update at least 3 different parsers (babel, core, server) to make a change.
    
    This change allows for declaring user-defined types at the root of a schema model and allows for easy type alias mapping. These data types are shared by all schema in the model so cast and DDL expressions do not need to scope data type references to a particular sub-schema.
    
    For example:
    ```
    inline: {
      version: '1.0',
      types: [
        {
          name: 'BOOL',
          type: 'BOOLEAN'
        },
        {
          name: 'BYTES',
          type: 'VARBINARY'
        },
    ...
      ],
    ```
    Allows for `CAST("true" as BOOL)`

--- core/src/main/java/org/apache/calcite/model/JsonRoot.java
+++ core/src/main/java/org/apache/calcite/model/JsonRoot.java
@@ -77,8 +77,6 @@ public class JsonRoot {
   /** List of types in the root schema.
    *
    * <p>Such types global, that is, shared by all schemas in the model.
-   *
-   * <p>The list may be empty.
    */
   public final List<JsonType> types = new ArrayList<>();
 
