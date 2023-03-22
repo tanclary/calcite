60e7b0fa9eb9b0d41ca3e3ae4908bf38dbe7b386
TJ Banghart <66929211+tjbanghart@users.noreply.github.com>
2023-03-22 12:32:06 -0700

    Revert "[CALCITE-5510] RelToSqlConverter should use ordinal for `ORDER BY` if the dialect allows" (#25)
    This reverts commit a990ecc4aee797f6d5a58cebcd18de38ae7a66cc.
    
    Causes too many failures for subtotals and pivots.

--- core/src/main/java/org/apache/calcite/rel/rel2sql/SqlImplementor.java
+++ core/src/main/java/org/apache/calcite/rel/rel2sql/SqlImplementor.java
@@ -619,13 +619,6 @@ public abstract class SqlImplementor {
         final String strValue = ((SqlNumericLiteral) node).toValue();
         return SqlLiteral.createCharString(strValue, node.getParserPosition());
       }
-      if (node instanceof SqlCall
-          && dialect.getConformance().isSortByOrdinal()) {
-        // If the field is expression and sort by ordinal is set in dialect,
-        // convert it to ordinal.
-        return SqlLiteral.createExactNumeric(
-            Integer.toString(ordinal + 1), SqlParserPos.ZERO);
-      }
       return node;
     }
 
@@ -1834,41 +1827,6 @@ public abstract class SqlImplementor {
         // underlying Project
         return true;
       }
-
-      if (rel instanceof Project
-          && clauses.contains(Clause.ORDER_BY)
-          && dialect.getConformance().isSortByOrdinal()
-          && hasSortByOrdinal()) {
-        // Cannot merge a Project that contains sort by ordinal under it.
-        return true;
-      }
-
-      if (rel instanceof Project) {
-        Project project = (Project) rel;
-        RelNode input = project.getInput();
-        if (input instanceof Aggregate) {
-          // Cannot merge because "select 1 from t"
-          // is different from "select 1 from (select count(1) from t)"
-          //
-          // Some databases don't allow "GROUP BY ()". On those databases,
-          //   SELECT MIN(1) FROM t
-          // is valid and
-          //   SELECT 1 FROM t GROUP BY ()
-          // is not. So, if an aggregate has no group keys we can only remove
-          // the subquery if there is at least one aggregate function.
-          // See RelToSqlConverter.buildAggregate.
-          final Aggregate aggregate = (Aggregate) input;
-          final ImmutableBitSet fieldsUsed =
-              RelOptUtil.InputFinder.bits(project.getProjects(), null);
-          final boolean hasAggregate =
-              !aggregate.getGroupSet().isEmpty()
-                  || !aggregate.getAggCallList().isEmpty();
-          if (hasAggregate && fieldsUsed.isEmpty()) {
-            return true;
-          }
-        }
-      }
-
       if (rel instanceof Aggregate) {
         final Aggregate agg = (Aggregate) rel;
         final boolean hasNestedAgg =
@@ -1886,30 +1844,6 @@ public abstract class SqlImplementor {
         }
       }
 
-      return false;
-    }
-
-    /**
-     * Return whether the current {@link SqlNode} in {@link Result} contains sort by column
-     * in ordinal format.
-     */
-    private boolean hasSortByOrdinal(@UnknownInitialization Result this) {
-      if (node instanceof SqlSelect) {
-        final SqlNodeList orderList = ((SqlSelect) node).getOrderList();
-        if (orderList == null) {
-          return false;
-        }
-        for (SqlNode sqlNode : orderList) {
-          if (!(sqlNode instanceof SqlBasicCall)) {
-            return sqlNode instanceof SqlNumericLiteral;
-          }
-          for (SqlNode operand : ((SqlBasicCall) sqlNode).getOperandList()) {
-            if (operand instanceof SqlNumericLiteral) {
-              return true;
-            }
-          }
-        }
-      }
       return false;
     }
 
